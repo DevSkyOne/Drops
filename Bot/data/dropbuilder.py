c183fbe1ea5280a42972652f3964961a9a33536b
CoasterFreakDE <mleaked@gmail.com>
2022-11-05 15:02:09 +0100

    Fixed link

--- Bot/data/dropbuilder.py
+++ Bot/data/dropbuilder.py
@@ -7,6 +7,6 @@ def build_drop_embed() -> Embed:
 		title=f'`        {i18n.t("drop.title")}        `',
 		color=0x2f3136
 	)
-	embed.set_image(url='https://cdn.discordapp.com/attachments/821000000000000000/821000000000000000/unknown.png')
+	embed.set_image(url='https://github.com/DevSkyOne/Drops/raw/main/assets/present.png')
 	embed.set_footer(text='© 2022 DropBot')
 	return embed

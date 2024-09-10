0b41aa6bb8cdeed5a8546ccaba57c4f2df14a581
Liam Sage <mleaked@gmail.com>
2024-09-10 10:05:30 +0200

    feat: #22 fixed copyright

--- Bot/data/dropbuilder.py
+++ Bot/data/dropbuilder.py
@@ -1,22 +1,24 @@
 import discord
 from discord import Embed, Button, ButtonStyle
 import i18n
+from datetime import datetime
 
 from Database.models import GuildData
 
-
 async def build_drop_embed(guild_id: int) -> Embed:
+	current_year = datetime.now().year
 	local: str = (await GuildData(guild_id).load()).language
 	embed = Embed(
 		title=f'`        {i18n.t("drop.title", locale=local)}        `',
 		color=0x2f3136
 	)
 	embed.set_image(url='https://github.com/DevSkyOne/Drops/raw/main/assets/present.png')
-	embed.set_footer(text='© 2023 DropBot')
+	embed.set_footer(text=f'© 2022-{current_year} DropBot')
 	return embed
 
 
 async def build_drop_embed_opened(user: discord.User, value: str, guild_id: int) -> Embed:
+	current_year = datetime.now().year
 	local: str = (await GuildData(guild_id).load()).language
 	embed = Embed(
 		title=f'`        {i18n.t("drop.opened.title", locale=local)}        `',
@@ -24,7 +26,7 @@ async def build_drop_embed_opened(user: discord.User, value: str, guild_id: int)
 	)
 	embed.add_field(name=i18n.t('drop.opened.field', locale=local), value=i18n.t('drop.opened.value', user=user.mention,
 	                                                                             value=value, locale=local))
-	embed.set_footer(text='© 2023 DropBot')
+	embed.set_footer(text=f'© 2022-{current_year} DropBot')
 	return embed
 
 
